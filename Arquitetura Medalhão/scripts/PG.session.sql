fdd75d3932f041b0e9eb067d61284f9cb8e685ab
Leticia Gama <dev.leticiacode@gmail.com>
2026-02-17 17:09:56 -0300

    Create PG.session.sql

---
+++ Arquitetura Medalhão/scripts/PG.session.sql
@@ -0,0 +1,19 @@
+SELECT 
+    users.id,
+    users.nome,
+    users.email,
+    users.data_nascimento,
+    users.genero,
+    users.cep,
+    cep_info.logradouro,
+    cep_info.complemento,
+    cep_info.unidade,
+    cep_info.uf, 
+    cep_info.estado,
+    cep_info.regiao,
+    cep_info.ibge,
+    cep_info.gia,
+    cep_info.ddd,
+    cep_info.siafi
+ FROM users
+INNER JOIN cep_info ON users.cep = cep_info.cep;
